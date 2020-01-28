0768666e51a3fb30b3cd91ccb3b3250c730fae70
ShashwatShah24 <shashwatm.shah@st.niituniversity.in>
2020-01-28 23:37:56 +0530

    first commit

---
+++ EmoloyeeDB.py
@@ -0,0 +1,14 @@
+import sqlite3  
+  
+con = sqlite3.connect("employee.db")  
+print("Database opened successfully")  
+  
+con.execute("create table Employees (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL,email TEXT UNIQUE NOT NULL, address TEXT NOT NULL)")  
+  
+print("Table created successfully")  
+  
+con.close()
+
+@app.route("/")  
+def index():  
+    return render_template("index.html");
