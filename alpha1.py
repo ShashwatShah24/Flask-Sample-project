e86c79359b474c6da1e1ba33a5c70e73f99c6be6
ShashwatShah24 <shashwatm.shah@st.niituniversity.in>
2020-02-07 23:16:33 +0530

    first commit

--- alpha1.py
+++ alpha1.py
@@ -3,13 +3,24 @@ import pandas as pd
 import sqlite3  
   
 app = Flask(__name__)  
- 
-@app.route("/")  
-def Stocks():
-    data = pd.read_excel('Book1.xlsx', sheet_name='Sheet1')
 
-    stocklist = list(data.values)
-    return render_template('simple.html', stocklist=stocklist)  
+@app.route("/")  
+def add():  
+    return render_template("index02.html")  
+ 
+@app.route("/savedetails",methods = ["POST"])  
+def saveDetails():  
+        try:  
+            name = request.form["sn"] 
+            data = pd.read_excel('Book1.xlsx', sheet_name='Sheet1')
+            rslt_df = data[data['a3'] == name]
+            stocklist = list(rslt_df.values)
+            return render_template('simple.html', stocklist=stocklist)
+        except:  
+            return "error"     
   
 if __name__ == "__main__":  
     app.run(debug = True)    
+
+
+#rslt_df = df[df['a3'] == 'q37']
